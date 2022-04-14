91100885d462bbf16c33343d9f187eba5af2bc58
Anatolich <anatolich.work.english@gmail.com>
2022-04-14 17:56:47 +0300

    Add postIndexView

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -8,33 +8,46 @@
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
     <title>Document</title>
 </head>
-<body class="font-sans bg-gray-900 text-white">
-    <nav class="border-b border-gray-600">
-        <div class="container mx-auto flex items-center justify-between px-4 py-6">
-            <ul class="flex items-center">
-                <li>
-                    <a href="#">Hello</a>
-                </li>
-                <li class="ml-16">
-                    <a href="#" class="hover:text-gray-300">Дорожная карта</a>
-                </li>
-                <li class="ml-6">
-                    <a href="#" class="hover:text-gray-300">Курсы</a>
-                </li>
-                <li class="ml-6">
-                    <a href="#" class="hover:text-gray-300">Модули</a>
-                </li>
-            </ul>
+<body class="font-sans bg-gray-900 text-white min-h-screen">
+    <div class="wrapper min-h-screen flex flex-col">
+        <nav class="border-b border-gray-600">
+            <div class="container flex items-center justify-between px-10 py-6">
+                <ul class="flex items-center">
+                    <li class="ml-10">
+                        <a href="#" class="flex items-center">
+                            <img src="{{asset('images/logo.svg')}}" class="w-10 h-10" alt="">
+                            <span>INIT</span>
+                        </a>
 
-            <div class="flex items-center">
-                <div class="relative">
-                    <input type="text" class="bg-gray-800 rounded-full w-64 px-4 pl-8 py-1
-                    focus:outline-none focus:shadow-outline" placeholder="Поиск">
+                    </li>
+                    <li class="ml-20">
+                        <a href="#" class="hover:text-gray-300">Дорожная карта</a>
+                    </li>
+                    <li class="ml-6">
+                        <a href="#" class="hover:text-gray-300">Модули</a>
+                    </li>
+                </ul>
+
+                <div class="flex items-center">
+                    <div class="ml-4">
+                        <a href="#" class="hover:text-gray-300">Профиль</a>
+                    </div>
                 </div>
             </div>
-        </div>
-    </nav>
-    <div class="block">@yield('content')</div>
-    <footer>Footer here</footer>
+        </nav>
+        <div class="content flex flex-3">@yield('content')</div>
+        <footer class="border-t border-gray-600 flex flex-4">
+            <div class="container flex justify-between items-center px-10 py-6">
+                <div class="flex items-center">
+                    <span>© 2022 Kim Anatoliy. Все права защищены</span>
+                </div>
+                <ul class="flex items-center">
+                    <li class="ml-6">Vk</li>
+                    <li class="ml-6">github</li>
+                    <li class="ml-6">gmail</li>
+                </ul>
+            </div>
+        </footer>
+    </div>
 </body>
 </html>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,18 @@ module.exports = {
     ],
     theme: {
         extend: {
+            container: {
+                screens: {
+                    sm: "100%",
+                    md: "100%",
+                    lg: "1024px",
+                    xl: "1920px"
+                },
+            },
+            flex: {
+                '3': '1 0 auto',
+                '4': '0 0 auto'
+            },
         },
     },
     plugins: [],
